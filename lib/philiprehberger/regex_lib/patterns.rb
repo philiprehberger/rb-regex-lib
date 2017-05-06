b4443533b6a06e865ce7c17f80685068dac31120
Philip Rehberger <me@philiprehberger.com>
2017-05-06 17:48:18 -0600

    Release v0.2.0

--- lib/philiprehberger/regex_lib/patterns.rb
+++ lib/philiprehberger/regex_lib/patterns.rb
@@ -2,14 +2,14 @@
 
 module Philiprehberger
   module RegexLib
-    # Email address (local@domain.tld)
-    EMAIL = %r{\A[a-zA-Z0-9.!\#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,}\z}
+    # Email address (local@domain.tld) with named captures
+    EMAIL = %r{\A(?<local>[a-zA-Z0-9.!\#$%&'*+/=?^_`{|}~-]+)@(?<domain>[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,})\z}
 
-    # HTTP/HTTPS URL
-    URL = %r{\Ahttps?://[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*(?::\d{1,5})?(?:/[^\s]*)?\z}
+    # HTTP/HTTPS URL with named captures
+    URL = %r{\A(?<scheme>https?)://(?<host>[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)(?::(?<port>\d{1,5}))?(?<path>/[^\s?#]*)?(?:\?(?<query>[^\s#]*))?(?:#(?<fragment>\S*))?\z}
 
-    # IPv4 address (0-255 per octet)
-    IPV4 = /\A(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\z/
+    # IPv4 address (0-255 per octet) with named captures
+    IPV4 = /\A(?<octet1>25[0-5]|2[0-4]\d|[01]?\d\d?)\.(?<octet2>25[0-5]|2[0-4]\d|[01]?\d\d?)\.(?<octet3>25[0-5]|2[0-4]\d|[01]?\d\d?)\.(?<octet4>25[0-5]|2[0-4]\d|[01]?\d\d?)\z/
 
     # IPv6 address (simplified: full, compressed, and loopback forms)
     IPV6 = /\A(?:(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?::[0-9a-fA-F]{1,4}){1,6}|:(?::[0-9a-fA-F]{1,4}){1,7}|::)\z/
@@ -47,6 +47,39 @@ module Philiprehberger
     # URL slug (lowercase alphanumeric and hyphens)
     SLUG = /\A[a-z0-9]+(?:-[a-z0-9]+)*\z/
 
+    # International Bank Account Number with named captures
+    IBAN = /\A(?<country>[A-Z]{2})(?<check>\d{2})(?<bban>[A-Z0-9]{1,30})\z/
+
+    # Valid domain name (labels separated by dots, TLD 2+ chars)
+    DOMAIN = /\A[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\.[a-zA-Z]{2,}\z/
+
+    # Unix file path (/foo/bar/baz.txt)
+    FILE_PATH_UNIX = %r{\A/(?:[^\0/]+/?)*\z}
+
+    # Windows file path (C:\foo\bar\baz.txt)
+    FILE_PATH_WINDOWS = Regexp.new('\A[a-zA-Z]:\\\\(?:[^\x00\\\\/:*?"<>|]+\\\\)*[^\x00\\\\/:*?"<>|]*\z')
+
+    # JSON Web Token (3 base64url segments separated by dots)
+    JWT = /\A[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+\z/
+
+    # Base64 encoded string (full match)
+    BASE64 = %r{\A(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?\z}
+
+    # Markdown link format [text](url) with named captures
+    MARKDOWN_LINK = /\A\[(?<text>[^\]]+)\]\((?<url>[^)]+)\)\z/
+
+    # Social media hashtag (#word)
+    HASHTAG = /\A#[a-zA-Z]\w*\z/
+
+    # Social media mention (@username)
+    MENTION = /\A@[a-zA-Z]\w*\z/
+
+    # Double-quoted JSON string with escape support
+    JSON_STRING = /\A"(?:[^"\\]|\\.)*"\z/
+
+    # HTML opening/closing tag
+    HTML_TAG = %r{\A</?[a-zA-Z][a-zA-Z0-9]*(?:\s+[a-zA-Z][a-zA-Z0-9-]*(?:\s*=\s*(?:"[^"]*"|'[^']*'|[^\s>]+))?)*\s*/?>}
+
     # Map of pattern names to constants for helper methods
     PATTERNS = {
       email: EMAIL,
@@ -63,7 +96,18 @@ module Philiprehberger
       ssn: SSN,
       mac_address: MAC_ADDRESS,
       semantic_version: SEMANTIC_VERSION,
-      slug: SLUG
+      slug: SLUG,
+      iban: IBAN,
+      domain: DOMAIN,
+      file_path_unix: FILE_PATH_UNIX,
+      file_path_windows: FILE_PATH_WINDOWS,
+      jwt: JWT,
+      base64: BASE64,
+      markdown_link: MARKDOWN_LINK,
+      hashtag: HASHTAG,
+      mention: MENTION,
+      json_string: JSON_STRING,
+      html_tag: HTML_TAG
     }.freeze
   end
 end
